c82caad24f652b2196b77820ac099d15d1ddeb2d
Michał Depta <mdepta@future-processing.com>
2017-03-10 12:42:40 +0100

    Added hosting configuration file

--- src/ALaCarte.Web/Program.cs
+++ src/ALaCarte.Web/Program.cs
@@ -1,4 +1,6 @@
-﻿using Topshelf;
+﻿using System.IO;
+using System.Reflection;
+using Topshelf;
 
 namespace ALaCarte.Web
 {
@@ -6,11 +8,13 @@ namespace ALaCarte.Web
     {
         public static void Main(string[] args)
         {
+            var contentRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             HostFactory.Run(config =>
             {
                 config.Service<WebHostingService>(service =>
                 {
-                    service.ConstructUsing(() => new WebHostingService());
+                    service.ConstructUsing(() => new WebHostingService(contentRoot));
                     service.WhenStarted(x => x.Start());
                     service.WhenStopped(x => x.Stop());
                 });
--- src/ALaCarte.Web/WebHostingService.cs
+++ src/ALaCarte.Web/WebHostingService.cs
@@ -1,20 +1,28 @@
 ﻿using System.Threading;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ALaCarte.Web
 {
     public class WebHostingService
     {
+        private readonly string _contentRoot;
         private readonly CancellationTokenSource _shutdownTokenSource = new CancellationTokenSource();
 
+        public WebHostingService(string contentRoot)
+        {
+            _contentRoot = contentRoot;
+        }
+
         public void Start() => new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(@"C:\Projects\github\ALaCarte\src\ALaCarte.Web")
-                //.UseContentRoot(Directory.GetCurrentDirectory()) // Changed by topshelf...
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build()
-                .Run(_shutdownTokenSource.Token);
+            .UseKestrel()
+            .UseContentRoot(_contentRoot)
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .UseConfiguration(
+                new ConfigurationBuilder().SetBasePath(_contentRoot).AddJsonFile("hosting.json", true).Build())
+            .Build()
+            .Run(_shutdownTokenSource.Token);
 
         public void Stop() => _shutdownTokenSource.Cancel();
     }
